fcba2855032994d8c395af000c33c5a671882c1b
wenfangcao <173581251@qq.com>
2018-05-23 09:50:23 +0800

    static

--- src/a.js
+++ src/a.js
@@ -1,1 +1,1 @@
-document.write('2222')
+document.write('2')
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin') //自动删除前次打包文件
 const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -15,23 +16,10 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /(\.jsx|\.js)$/,
-        use: {
-          loader: "babel-loader"
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          }, {
-            loader: "css-loader"
-          }
-        ]
-      },
+      {test: /(\.jsx|\.js)$/,use: {loader: "babel-loader"},exclude: /node_modules/},
+      {test: /\.css$/,use: [{loader: "style-loader"}, {loader: "css-loader"}]},
+      {test:/\.vue$/,loader:'vue-loader'},
+      {test: /\.(png|jpg|gif)$/, loader: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'},
     ]
   },
   resolve: {
@@ -46,15 +34,21 @@ module.exports = {
     new CleanWebpackPlugin(
       ['dist/main.*.js','dist/manifest.*.js',],　 //匹配删除的文件
       {
-          root: __dirname,       　　　　　　　　　　//根目录
-          verbose:  true,        　　　　　　　　　　//开启在控制台输出信息
-          dry:      false        　　　　　　　　　　//启用删除文件
+        root: __dirname,       　　　　　　　　　　//根目录
+        verbose:  true,        　　　　　　　　　　//开启在控制台输出信息
+        dry:      false        　　　　　　　　　　//启用删除文件
       }
     ),
     new OpenBrowserPlugin({
       url: 'http://localhost:1212'
     }),
-    // new webpack.HotModuleReplacementPlugin()
+    new CopyWebpackPlugin([      //忽略static文件夹的编译
+      {
+        from: path.resolve(__dirname, 'static'),
+        to: 'static',
+        ignore: ['.*']
+      }
+    ]),
   ],
   devtool:'eval-source-map',
   devServer: {
